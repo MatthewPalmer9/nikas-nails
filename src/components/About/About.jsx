6c25838590bad4d7239380f6562bc008e462210e
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-13 08:54:01 -0500

    Final commit for this branch. Accidentally built features beyond the navbar.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,15 @@ export default function About() {
   return (
     <section className="about-section">
         <div className="container">
-        
+            <div className="about-content">
+                <div className="left-side">
+                    <header className="header-title"><em>About Nika</em><span>.</span></header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium nesciunt qui odit blanditiis sequi unde quibusdam. Sunt, animi ut dolor officiis eligendi eius iusto a perspiciatis debitis, facilis nostrum nesciunt dolorem error quasi ratione explicabo hic ullam adipisci. Asperiores nostrum suscipit aliquam amet omnis temporibus ullam obcaecati exercitationem neque, accusantium excepturi sed nesciunt, odit consectetur deleniti iste! Laudantium sed, incidunt quo odio eaque dignissimos illum alias autem laboriosam omnis quis inventore tempora eum minima quidem perspiciatis saepe culpa eos ab, totam ipsum nihil vitae? Porro, minima ullam ea mollitia sit eum. Consequatur commodi accusantium nesciunt. Autem blanditiis vitae sapiente.</p>
+                </div>
+                <div className="right-side">
+                    
+                </div>
+            </div>
         </div>
     </section>
   )
