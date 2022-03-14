7467447c9e15ebe9ec79c325f49e7cc33dd1608e
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-14 10:55:28 -0500

    Changed styling and mobile responsive behavior in both the Hero and About section.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,10 @@ export default function About() {
             <div className="about-content">
                 <div className="left-side">
                     <header className="header-title"><em>About Nika</em><span>.</span></header>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium nesciunt qui odit blanditiis sequi unde quibusdam. Sunt, animi ut dolor officiis eligendi eius iusto a perspiciatis debitis, facilis nostrum nesciunt dolorem error quasi ratione explicabo hic ullam adipisci. Asperiores nostrum suscipit aliquam amet omnis temporibus ullam obcaecati exercitationem neque, accusantium excepturi sed nesciunt, odit consectetur deleniti iste! Laudantium sed, incidunt quo odio eaque dignissimos illum alias autem laboriosam omnis quis inventore tempora eum minima quidem perspiciatis saepe culpa eos ab, totam ipsum nihil vitae? Porro, minima ullam ea mollitia sit eum. Consequatur commodi accusantium nesciunt. Autem blanditiis vitae sapiente.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet laudantium nesciunt qui odit blanditiis sequi unde quibusdam. Sunt, animi ut dolor officiis eligendi eius iusto a perspiciatis debitis, facilis nostrum nesciunt dolorem error quasi ratione explicabo hic ullam adipisci. Asperiores nostrum suscipit aliquam amet omnis temporibus ullam obcaecati exercitationem neque, accusantium excepturi sed nesciunt, odit consectetur deleniti iste!</p>
                 </div>
                 <div className="right-side">
-                    
+                    <img src="./nika.jpg" alt="" />
                 </div>
             </div>
         </div>
